885014c2f7c029bc8cccbcb7b8879a7576865192
geno <gabrigeno@gmail.com>
2022-08-23 17:47:49 +0200

    ce se prova

--- front-office/src/App.js
+++ front-office/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Navbar />
+            <h1>CasAnimale</h1>
+            <h5>Dove il tuo amico animale si sente a casa</h5>
+            <Footer />
+        </>
+    )
 }
 
-export default App;
+export default App
