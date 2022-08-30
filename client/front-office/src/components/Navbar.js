07a18bf89ff07b1a73d187f2a9ac2aac39301abc
geno <gabrigeno@gmail.com>
2022-08-30 16:55:44 +0200

    migliori footer e navbar

--- client/front-office/src/components/Navbar.js
+++ client/front-office/src/components/Navbar.js
@@ -2,83 +2,62 @@ import React from "react";
 
 // non va, da controllare
 const manageMenu = () => {
-  const vis = document.querySelector('#menu').style.visibility
-  console.log(vis)
-  if(vis == 'visible') {
-    console.log("Lo rendo invisibile")
-    document.querySelector('#menu').style.visibility = 'hidden'
+  const vis = document.querySelector("#menu").style.visibility;
+  console.log(vis);
+  if (vis === "visible") {
+    console.log("Lo rendo invisibile");
+    document.querySelector("#menu").style.visibility = "hidden";
   } else {
-    console.log("Lo rendo visibile")
-    document.querySelector('#menu').style.visibility = 'visible'
+    console.log("Lo rendo visibile");
+    document.querySelector("#menu").style.visibility = "visible";
   }
-
-}
+};
 
 const Navbar = () => {
-  
   return (
-    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <svg
-          class="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
-        <span class="font-semibold text-xl tracking-tight">CasAnimale</span>
+    <div class="navbar bg-blue-100">
+      <div class="flex-1">
+        <a href="http://localhost:3000" class="btn btn-ghost normal-case text-xl">CasAnimale</a>
       </div>
-      <div class="block lg:hidden">
-        <button onClick={manageMenu} class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg
-            class="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal p-0">
+          <li>
+            <a href="http://localhost:5173" target="_blank">Area giochi</a>
+          </li>
+          <li>
+            <a>Area riservata</a>
+          </li>
+          <li tabindex="0">
+            <a>
+              Servizi
+              <svg
+                class="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul class="p-2 bg-base-100">
+              <li>
+                <a>eCommerce</a>
+              </li>
+              <li>
+                <a>Psicologo</a>
+              </li>
+              <li>
+                <a>Negozi</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a>Registrati</a>
+          </li>
+        </ul>
       </div>
-      <div id="menu" class="w-full block flex-grow sm:hidden md:hidden lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-          <a
-            href="http://localhost:5173/"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Giochi
-          </a>
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Servizi
-          </a>
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-          >
-            Area riservata
-          </a>
-        </div>
-        <div>
-          <a
-            href="#"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 left-5"
-          >
-            Sign in
-          </a>
-          <a
-            href="#"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-          >
-            Registrati
-          </a>
-        </div>
-      </div>
-    </nav>
+    </div>
   );
 };
 
