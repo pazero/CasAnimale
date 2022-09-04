b1064f9267daa588c24f01b9d9a3f97b474bf930
geno <gabrigeno@gmail.com>
2022-09-04 12:47:54 +0200

    preparazione register form

--- client/front-office/src/views/Register.js
+++ client/front-office/src/views/Register.js
@@ -1,8 +1,20 @@
 import React from "react";
+import UserManage from "../services/UserManage";
 
 const Footer = () => {
+  const sendData = async (data) => {
+    data.preventDefault();
+    UserManage.register(data).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
-    <div><h1>Form di registrazione</h1></div>
+    <div className="">
+      <form onSubmit={sendData}>
+        <h1>Form di registrazione</h1>
+      </form>
+    </div>
   );
 };
 
