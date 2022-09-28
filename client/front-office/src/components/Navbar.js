cd843614442fb3316e1eb5ba86d24ac502214b5b
geno <gabrigeno@gmail.com>
2022-09-28 10:54:14 +0200

    navbar fixed

--- client/front-office/src/components/Navbar.js
+++ client/front-office/src/components/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a>Area Giochi</a>
+              <a href="http://localhost:5237">Area Giochi</a>
             </li>
             <li tabIndex={0}>
               <a className="justify-between">
@@ -44,7 +44,7 @@ const Navbar = () => {
                   <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
                 </svg>
               </a>
-              <ul className="p-2">
+              <ul className="p-2 border bg-base-100 z-10">
                 <li>
                   <a>eCommerce</a>
                 </li>
@@ -63,22 +63,22 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
           <li>
-            <a>Area Giochi</a>
+            <a href="http://localhost:5237">Area Giochi</a>
           </li>
           <li tabIndex={0}>
-            <a>
+            <a className="justify-between">
               Servizi
               <svg
                 className="fill-current"
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width="24"
+                height="24"
                 viewBox="0 0 24 24"
               >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
               </svg>
             </a>
-            <ul className="p-2">
+            <ul className="p-2 border bg-base-100 z-10">
               <li>
                 <a>eCommerce</a>
               </li>
