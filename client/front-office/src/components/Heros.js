66a4451da78a3be8f60fc0288efdf3a4f83de95e
Paolo Ceroni <paoloceroni01@gmail.com>
2022-09-29 13:26:30 +0200

    poi andrà meglio

--- client/front-office/src/components/Heros.js
+++ client/front-office/src/components/Heros.js
@@ -42,24 +42,24 @@ export default function CaptionCarousel() {
   // This can be static or loaded from a server
   const cards = [
     {
-      //display: 'flex',
-      //height: '100%',
+      display: 'flex',
+      height: '100%',
       title: "Design Projects 1",
       text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
         "https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
     },
     {
-      //display: 'flex',
-      //height: '100%',
+      display: 'flex',
+      height: '100%',
       title: "Design Projects 2",
       text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
         "https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80",
     },
     {
-      //display: 'flex',
-      //height: '100%',
+      display: 'flex',
+      height: '100%',
       title: "Design Projects 3",
       text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
