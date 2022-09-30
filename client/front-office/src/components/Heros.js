bf2196fb201a21825a36548f292d6dc9fad1e2e6
Paolo Ceroni <paoloceroni01@gmail.com>
2022-09-30 16:44:31 +0200

    improve

--- client/front-office/src/components/Heros.js
+++ client/front-office/src/components/Heros.js
@@ -44,21 +44,21 @@ export default function CaptionCarousel() {
       text:
         "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
-      'cane-veterinario.jpg',
+      'vet3.jpg',
     },
     {
       title: 'Psychologist service',
       text:
         "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
-        'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
+        'p2.jpg',
     },
     {
-      title: 'eCommerce',
+      title: 'Pet shop',
       text:
         "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
-        'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        "pet_toys.jpg",
     },
   ];
 
@@ -117,7 +117,7 @@ export default function CaptionCarousel() {
             backgroundImage={`url(${card.image})`}>
             {/* This is the block you need to change, to customize the caption */}
             <Container display={"flex"} alignItems={"center"} size="container.lg" height="75vh" position="relative">
-              <div position="absolute" top="50vh" className="bg-gray-300 bg-opacity-60 p-2 rounded">
+              <div position="absolute" top="50vh" className="bg-gray-300 bg-opacity-85 p-2 rounded">
                 <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }} className="text-black pb-6" style={{color:"black"}}>
                   {card.title}
                 </Heading>
