c7a88b0e77d1149edf52e0fe4d4ddd972fa79800
Paolo Ceroni <paoloceroni01@gmail.com>
2022-09-30 13:30:43 +0200

    penso sia srl, non slr <3

--- client/front-office/src/views/Home.js
+++ client/front-office/src/views/Home.js
@@ -7,7 +7,7 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <div data-theme="lemonade" className="App flex h-screen flex-1" style={{flexDirection:"column", justifyContent:"space-between", maxHeight:"100%"}}>
-      <div className="flex flex-1" style={{height:"3rem",maxHeight:"4rem"}}>
+      <div className="flex flex-1" style={{height:"4rem",maxHeight:"4rem"}}>
         <Navbar />
       </div>
       <div className="flex flex-1" style={{height:"auto"}}>
