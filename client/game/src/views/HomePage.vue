4f2b15d4cea8bf81f9a20aa542ca76ea3204e0cd
Paolo Ceroni <paoloceroni01@gmail.com>
2022-11-07 12:56:21 +0100

    fix/responsive banner, not sm

--- client/game/src/views/HomePage.vue
+++ client/game/src/views/HomePage.vue
@@ -26,30 +26,30 @@ const games = [
 ]
 
 const adsLeft = [
-  { name: 'First - site', href: 'http://localhost:3000', bg:'bg-black', height:"height: 33%;", current: false},
-  { name: 'Second - quiz', href: 'http://localhost:5173/quiz', bg:'bg-blue-800', height:"height: 34%;", current: false},
-  { name: 'Third - hangman', href: 'http://localhost:5173/hangman', bg:'bg-indigo-300', height:"height: 33%;", current: false},
+  { name: 'First - site', href: 'http://localhost:3000', bg:'bg-black', current: false},
+  { name: 'Second - quiz', href: 'http://localhost:5173/quiz', bg:'bg-blue-800', current: false},
+  { name: 'Third - hangman', href: 'http://localhost:5173/hangman', bg:'bg-indigo-300',  current: false},
 ]
 
 const adsRight = [
-  { name: 'First - site', href: 'http://localhost:3000', bg:'bg-black', height:"height: 33%;", current: false},
-  { name: 'Second - quiz', href: 'http://localhost:5173/quiz', bg:'bg-blue-800', height:"height: 34%;", current: false},
-  { name: 'Third - hangman', href: 'http://localhost:5173/hangman', bg:'bg-indigo-300', height:"height: 33%;", current: false},
+  { name: 'First - site', href: 'http://localhost:3000', bg:'bg-black', current: false},
+  { name: 'Second - quiz', href: 'http://localhost:5173/quiz', bg:'bg-blue-800', current: false},
+  { name: 'Third - hangman', href: 'http://localhost:5173/hangman', bg:'bg-indigo-300',  current: false},
 ]
 </script>
 
 <template>
-  <div class="flex flex-row h-full" style="flex:1 1 auto;" data-theme="lemonade">
-      <div class="hidden flex-col text-white lg:flex" style="min-width: 15%">
-        <a v-for="item in adsLeft" :key="item.name" :href="item.href" :class="[item.bg,'hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium']" :style="[item.height,'border-radius:0']" :aria-current="undefined">{{ item.name }}</a>
+  <div class="flex xl:flex-row flex-col h-full" style="flex:1 1 auto;" data-theme="lemonade">
+      <div class="flex xl:flex-col xl:h-full flex-row text-white" style="flex:1 1 auto; min-width: 15%; min-width: 15%">
+        <a v-for="item in adsLeft" :key="item.name" :href="item.href" :class="[item.bg,'hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium']" :style="[item.height,'border-radius:0','flex-grow:1']" :aria-current="undefined">{{ item.name }}</a>
       </div>
       
-      <div class="mx-auto max-w-2xl py-4 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div class="mx-auto max-w-2xl py-4 px-4 sm:py-8 sm:px-6 xl:max-w-7xl xl:px-8">
         <div class="py-4 pl-3 font-semibold normal-case text-xl">
           GAMES
         </div>
         
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
           <a v-for="game in games" :key="game.id" :href="game.href" class="card">
             <div class="card-body bg-gray-100" style="padding:2rem; margin:1rem;">
               <p class="card-title hidden" >{{ game.name }}</p>
@@ -59,8 +59,8 @@ const adsRight = [
         </div>
       </div>
       
-      <div class="hidden flex-col text-white lg:flex" style="min-width: 15%">
-        <a v-for="item in adsLeft" :key="item.name" :href="item.href" :class="[item.bg,'hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium']" :style="[item.height,'border-radius:0']" :aria-current="undefined">{{ item.name }}</a>
+      <div class="flex xl:flex-col xl:h-full flex-row text-white" style="flex:1 1 auto; min-width: 15%; min-width: 15%">
+        <a v-for="item in adsRight" :key="item.name" :href="item.href" :class="[item.bg,'hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium']" :style="[item.height,'border-radius:0',,'flex-grow:1']" :aria-current="undefined">{{ item.name }}</a>
       </div>
   </div>
 </template>
