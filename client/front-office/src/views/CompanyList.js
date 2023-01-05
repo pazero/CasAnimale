4b30d561c4cb9df562e70fc8b369e298eac4e734
Paolo Ceroni <paoloceroni01@gmail.com>
2023-01-05 18:07:40 +0100

    good specialist pages

--- client/front-office/src/views/CompanyList.js
+++ client/front-office/src/views/CompanyList.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import CompanyManage from "../services/CompanyManage";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+
 import CompanyShowcase from "../components/CompanyShowcase";
 
 const CompanyList = (props) => {
+  const navigate = useNavigate();
   const [company, setCompany] = useState([]);
   let title = props.type==="vet" ? "Veterinary":"Pet sitter"
   useEffect(() => {
@@ -19,28 +22,62 @@ const CompanyList = (props) => {
   return (
     <div
       data-theme="lemonade"
-      className="flex h-screen flex-1"
+      className="flex h-full flex-1 flex-col justify-start"
       style={{
-        flexDirection: "column",
-        justifyContent: "space-between",
         maxHeight: "100%",
       }}
     >
       <div
-        className="flex flex-1"
+        className="flex flex-1 self-place-start"
         style={{ height: "4rem", maxHeight: "4rem" }}
       >
         <Navbar />
       </div>
-      <div id="listTitle" class="font-bold">
-          {title}
+      <div className="flex flex-col h-full">
+        <div id="listTitle" class="my-8 text-6xl self-center font-bold uppercase">
+            {title}
+        </div>
+        <div className="flex flex-wrap justify-center" style={{flex:"0 1 auto" }}>
+          {company.map((product) => (
+            //<CompanyShowcase data={product}/>
+            <a
+              id={product._id}
+              className="flex flex-col flex-wrap p-2 m-4 mt-0 bg-[#f0f2f3] hover:bg-[#b9b9ff] rounded-lg"
+              style={{ border: "solid 1px #191A3E", color: "#191A3E", minWidth: "20rem"}}
+              onClick={() => {navigate("/specialistpage/"+product._id);}}
+            >
+              <div>
+                <div className="mb-2 rounded font-semibold text-2xl text-center" style={{color: "#191A3E"}}>
+                  {product.name}
+                </div>
+                
+                <div className="mb-2 mx-2">
+                  <p className="font-semibold">Description: </p>
+                  <div className="ml-4" style={{overflowWrap:"break-word", inlineSize:"20rem"}}>{product.description}</div>
+                </div>
+                
+                <div className="mb-2 mx-2">
+                  <p className=" font-semibold">Doctor: </p>
+                  <p className="ml-4" style={{overflowWrap:"break-word", inlineSize:"20rem"}}>{product.owner}</p>
+                </div>
+                
+                <div className="mb-2 mx-2">
+                <p className="font-semibold">Cities: </p>
+                <ul className="ml-4" style={{overflowWrap:"break-word", inlineSize:"20rem"}}> {product.cities.map((item) => (<li>{item}</li>))}</ul>
+                </div>
+              </div>
+              <div className="flex items-end flex-1">
+                <div className="flex flex-row mb-2 mx-2 font-semibold">
+                  <div className="mr-4 self-center" >Opened from {product.business_hours.start} to {product.business_hours.end}</div>
+                  <div className="px-2 py-1 rounded flex self-center justify-center flex-start font-semibold bg-[#191A3E]" style={{color:"#b9b9ff"}}>
+                    {product.cost_per_hour} € / h
+                  </div>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
       </div>
-      <div className="flex flex-1" style={{ height: "auto" }}>
-        {company.map((product) => (
-          <CompanyShowcase data={product}/>
-        ))}
-      </div>
-
       <div className="flex">
         <Footer />
       </div>
