f4b289510afb7850750761aaca2b72fe16193a15
Fedegri <federica.grisendi@studio.unibo.it>
2023-01-05 15:07:10 +0100

    fix card dello shop

--- client/game/src/components/Services.vue
+++ client/game/src/components/Services.vue
@@ -9,7 +9,7 @@
           <img src="vet.png" alt="vet icon" class="self-center w-20 h-20 mr-4 object-center group-hover:opacity-75" />
           <div class="flex flex-col md:flex-row ml-2">
             <h2 class="mb-2 tracking-tight text-gray-900">Vet&emsp;</h2>
-            <p class="font-normal text-gray-700 0">
+            <p class="font-normal text-gray-700">
               Here you can find all the vets we work with. You can book a visit with a vet or just read info about it. Take a look!
             </p>
           </div>
@@ -19,10 +19,10 @@
     <div class="flex justify-center mt-4 font-semibold">
       <a href="http://localhost:3000/forum" class="flex-initial max-w-sm sm:max-w-xl md:max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
         <div class="flex flex-row">
-          <img src="forum.png" alt="vet icon" class="self-center w-20 h-20 mr-3 object-center group-hover:opacity-75" />
+          <img src="forum.png" alt="vet icon" class="self-center w-20 h-20 mr-4 object-center group-hover:opacity-75" />
           <div class="flex flex-col md:flex-row ml-2">
             <h2 class="mb-2 tracking-tight text-gray-900">Forum&emsp;</h2>
-            <p class="font-normal text-gray-700 0">
+            <p class="font-normal text-gray-700">
               Read here some other users' posts. Maybe you can find some useful tips and tricks. Take a look!
             </p>
           </div>
@@ -32,10 +32,10 @@
     <div class="flex justify-center mt-4 font-semibold">
       <a href="http://localhost:3000/petsitter" class="flex-initial max-w-sm sm:max-w-xl md:max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
         <div class="flex flex-row">
-          <img src="petsitter.png" alt="vet icon" class="self-center w-20 h-20 mr-3 object-center group-hover:opacity-75" />
+          <img src="petsitter.png" alt="vet icon" class="self-center w-20 h-20 mr-4 object-center group-hover:opacity-75" />
           <div class="flex flex-col md:flex-row ml-2">
             <h2 class="mb-2 tracking-tight text-gray-900">Petsitter&emsp;</h2>
-            <p class="font-normal text-gray-700 0">
+            <p class="font-normal text-gray-700">
               Here you can find all the petsitters we work with. You can reserve a prenotation with a pet-sitter or just read info about it. Have a look!
             </p>
           </div>
@@ -45,10 +45,10 @@
     <div class="flex justify-center mt-4 font-semibold">
       <a href="http://localhost:3000/psychologist" class="flex-initial max-w-sm sm:max-w-xl md:max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
         <div class="flex flex-row">
-          <img src="psychologist.png" alt="vet icon" class="self-center w-20 h-20 mr-3 object-center group-hover:opacity-75" />
+          <img src="psychologist.png" alt="vet icon" class="self-center w-20 h-20 mr-4 object-center group-hover:opacity-75" />
           <div class="flex flex-col md:flex-row ml-2">
             <h2 class="mb-2 tracking-tight text-gray-900">Psychologist&emsp;</h2>
-            <p class="font-normal text-gray-700 0">
+            <p class="font-normal text-gray-700">
               Here you can find all the psychologists we work with. You can reserve a prenotation with one of them or just read info about it. Have a look!
             </p>
           </div>
@@ -58,10 +58,10 @@
     <div class="flex justify-center mt-4 font-semibold">
       <a href="http://localhost:3000/compra" class="flex-initial max-w-sm sm:max-w-xl md:max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
         <div class="flex flex-row">
-          <img src="compra.png" alt="vet icon" class="self-center w-20 h-20 mr-3 object-center group-hover:opacity-75" />
+          <img src="compra.png" alt="vet icon" class="self-center w-20 h-20 mr-4 object-center group-hover:opacity-75" />
           <div class="flex flex-col md:flex-row ml-2">
-            <h2 class="mb-2 tracking-tight text-gray-900">E-commerce&emsp;</h2>
-            <p class="font-normal text-gray-700 0">
+            <h2 class="mb-2 tracking-tight text-gray-900">Shop&emsp;</h2>
+            <p class="font-normal text-gray-700">
               Do you need any supplies for your pets? Here you can find everything you may need! Take a look!
             </p>
           </div>
@@ -71,10 +71,10 @@
     <div class="flex justify-center mt-4 font-semibold">
       <a href="http://localhost:3000/grooming" class="flex-initial max-w-sm sm:max-w-xl md:max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
         <div class="flex flex-row">
-          <img src="grooming.png" alt="vet icon" class="self-center w-20 h-20 mr-3 object-center group-hover:opacity-75" />
+          <img src="grooming.png" alt="vet icon" class="self-center w-20 h-20 mr-4 object-center group-hover:opacity-75" />
           <div class="flex flex-col md:flex-row ml-2">
             <h2 class="mb-2 tracking-tight text-gray-900">Grooming&emsp;</h2>
-            <p class="font-normal text-gray-700 0">
+            <p class="font-normal text-gray-700">
               Here you can find all the groomers we work with. You can reserve a prenotation with one of them or just read info about it. Take a look!
             </p>
           </div>
