5e7ee4b0865df4b790c40b1b15b461c1c09f7224
Paolo Ceroni <paoloceroni01@gmail.com>
2023-01-13 20:56:34 +0100

    show personal prenotation ordereb by closest to furthest

--- client/front-office/src/components/PrenotationsList.js
+++ client/front-office/src/components/PrenotationsList.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from "react";
 import PrenotationManage from "../services/PrenotationManage";
 import CompanyManage from "../services/CompanyManage";
-import { Box, Heading, Text, Container, Button} from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Button,
+  Stack,
+  Center,
+  Image,
+} from "@chakra-ui/react";
 
 const PrenotationsList = () => {
   const [prenotationsList, setPrenotationsList] = useState([]);
   const [isPrenotationEmpty, setisPrenotationEmpty] = useState();
-
+  
   useEffect(() => {
     async function fetchData() {
       const ret = await PrenotationManage.getPrenotations();
       const ptList = ret.data;
+      ptList.sort(function (a, b) {
+        var keyA = new Date(a.start),
+          keyB = new Date(b.start);
+        // Compare the 2 dates
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+      });
       setPrenotationsList(
         await Promise.all(
           ptList.map((item) => {
@@ -32,6 +49,12 @@ const PrenotationsList = () => {
     return await Promise.all(ret.data.name);
   }
 
+  function calcTime(time) {
+    let start_hours = +time > 12 ? (+time % 12) + "pm" : time + "am";
+    let end_hours =
+      +time + 1 > 12 ? ((+time + 1) % 12) + "pm" : +time + 1 + "am";
+    return start_hours + " - " + end_hours;
+  }
   return (
     <Container maxW="100%">
       <Container maxW={"7xl"} p="15" pt="10" mt="4">
@@ -63,25 +86,49 @@ const PrenotationsList = () => {
                   backgroundColor="gray.50"
                   borderRadius="md"
                 >
-                  <Heading as="h2" marginTop="1">
-                    <div>{item.start
-                          ? item.start.substring(5, 7) +
-                            "/" +
-                            item.start.substring(8, 10) +
-                            "/" +
-                            item.start.substring(0, 4)
-                          : item.start}</div>
-                  </Heading>
-                  <Text as="p" marginTop="2" fontSize="lg">
-                    <ul>
-                      <li>Company: {}</li>
-                      <li>
-                        Schedule:{" "}
-                        {item.start ? item.start.substring(11, 16) : item.start}
-                      </li>
-                    </ul>
-                  </Text>
-              <Button></Button>
+                  <Stack direction={["column", "row"]} spacing={6}>
+                    <Center>
+                      <Image
+                        h={"7rem"}
+                        w={"7rem"}
+                        ml={{ base: "0", sm: "10" }}
+                        src={item.photo}
+                      />
+                    </Center>
+                    <Box w={"20rem"} pl={{ base: 10 }}>
+                      <Heading as="h2" marginTop="1">
+                        <div>
+                          {item.start
+                            ? item.start.substring(5, 7) +
+                              "/" +
+                              item.start.substring(8, 10) +
+                              "/" +
+                              item.start.substring(0, 4)
+                            : item.start}
+                        </div>
+                      </Heading>
+                      <Text as="p" marginTop="2" fontSize="lg">
+                        <ul>
+                          <li>Company: {}</li>
+                          <li>
+                            Schedule: {calcTime(item.start?.substring(11, 13))}
+                          </li>
+                        </ul>
+                      </Text>
+                    </Box>
+                    <Box align={"center"}>
+                      <Button
+                        rounded={"full"}
+                        mt={{ base: "0", sm: "12" }}
+                        mb={{ base: "3", sm: "12" }}
+                        bg={"red.400"}
+                        _hover={{ bg: "red.500" }}
+                        //onClick={() => deleteP(item._id)}
+                      >
+                        Delete
+                      </Button>
+                    </Box>
+                  </Stack>
                 </Box>
               </Box>
             </Box>
@@ -94,3 +141,37 @@ const PrenotationsList = () => {
 };
 
 export default PrenotationsList;
+
+/*
+<Heading as="h2" marginTop="1">
+                    <div>
+                      {item.start
+                        ? item.start.substring(5, 7) +
+                          "/" +
+                          item.start.substring(8, 10) +
+                          "/" +
+                          item.start.substring(0, 4)
+                        : item.start}
+                    </div>
+                  </Heading>
+                  <Text as="p" marginTop="2" fontSize="lg">
+                    <ul>
+                      <li>Company: {}</li>
+                      <li>
+                        Schedule:{" "}
+                        {item.start ? item.start.substring(11, 16) : item.start}
+                      </li>
+                    </ul>
+                  </Text>
+                </Box>
+                <Box align={"center"}>
+                  <Button
+                    rounded={"full"}
+                    mt={{ base: "0", sm: "12" }}
+                    mb={{ base: "3", sm: "12" }}
+                    bg={"red.400"}
+                    _hover={{ bg: "red.500" }}
+                  >
+                    Delete
+                  </Button>
+                </Box>*/
