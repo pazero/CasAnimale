d34f3a89742ccdeb0caa52356232909974625019
Paolo Ceroni <paoloceroni01@gmail.com>
2023-01-18 13:28:42 +0100

    sidebar

--- server/src/back-office/script/index.js
+++ server/src/back-office/script/index.js
@@ -1,5 +1,8 @@
 // usare jQuery
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
 
-function welcome() {
-    alert("Ciao")
+function Navbar() {
+    $("#navbar").innerHTML = `<div> CIAO BELLO</div>`;
+    alert("ciao geno");
 }
