b4b3ab5446967779b35f7628b77ce82132f1ccc8
Paolo Ceroni <paoloceroni01@gmail.com>
2023-01-18 13:51:42 +0100

    titolooo

--- server/src/back-office/script/index.js
+++ server/src/back-office/script/index.js
@@ -1,8 +1,3 @@
 // usare jQuery
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
-
-function Navbar() {
-    $("#navbar").innerHTML = `<div> CIAO BELLO</div>`;
-    alert("ciao geno");
-}
