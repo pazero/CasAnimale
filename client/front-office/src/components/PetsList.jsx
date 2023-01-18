1615f3a33f06d65f83a3dd4eaa07243d0b05de0f
geno <gabriele.genovese2@studio.unibo.it>
2023-01-18 18:34:50 +0100

    updated todo

--- client/front-office/src/components/PetsList.jsx
+++ client/front-office/src/components/PetsList.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import PetManage from "../services/PetManage";
 import UserManage from "../services/UserManage";
-import { Image, Stack, Flex, Center, Button, Box, Heading, Text, Container } from "@chakra-ui/react";
-
-
+import {
+  Image,
+  Stack,
+  Center,
+  Button,
+  Box,
+  Heading,
+  Text,
+  Container,
+} from "@chakra-ui/react";
 
 const PetsList = () => {
   const [petsList, setPetList] = useState([]);
@@ -13,16 +20,17 @@ const PetsList = () => {
 
   useEffect(() => {
     async function getUserId() {
-      var ret = await UserManage.getUser()
-      setUserId(ret.data._id)
+      var ret = await UserManage.getUser();
+      setUserId(ret.data._id);
     }
 
     async function fetchData() {
-      const ret = await PetManage.getPets({owner: userId});
+      const ret = await PetManage.getPets({ owner: userId });
       const ptList = ret.data;
       setPetList(
         await Promise.all(
-          ptList.map((item) => {    // item = pets
+          ptList.map((item) => {
+            // item = pets
             setIsPetFull(true);
             return {
               ...item,
@@ -37,7 +45,7 @@ const PetsList = () => {
   }, [userId]);
 
   async function deleteP(pet) {
-    console.log(pet)
+    console.log(pet);
     const ret = await PetManage.deletePet(pet);
     alert(ret.data.message);
     window.location.reload();
@@ -47,77 +55,88 @@ const PetsList = () => {
     <Container maxW="100%">
       <Container maxW={"7xl"} p="15" pt="10">
         <Heading as="h1">Your pets</Heading>
-        {(isPetFull && endList) ? (
-        <Text>
-          {petsList.map((item, i) => (
-            <Box
-              key = {i}
-              marginTop={{ base: "1", sm: "5" }}
-              display="flex"
-              flexDirection={{ base: "column", sm: "row" }}
-              justifyContent="space-between"
-            >
+        {isPetFull && endList ? (
+          <Text>
+            {petsList.map((item, i) => (
               <Box
+                key={i}
+                marginTop={{ base: "1", sm: "5" }}
                 display="flex"
-                flex="1"
-                marginRight="3"
-                position="relative"
+                flexDirection={{ base: "column", sm: "row" }}
+                justifyContent="space-between"
               >
                 <Box
                   display="flex"
                   flex="1"
-                  flexDirection="column"
-                  justifyContent="center"
-                  marginTop={{ base: "3", sm: "0" }}
-                  padding="2"
-                  paddingTop="3"
-                  paddingRight="0"
-                  backgroundColor="gray.50"
-                  borderRadius="md"
+                  marginRight="3"
+                  position="relative"
                 >
-                  <Stack direction={['column', 'row']} spacing={6}>
-                    <Center>
-                      <Image h={'7rem'} w={'7rem'} ml={{ base: '0', sm: '10' }} src={(item.photo === "") ? "/speciesIcons/" + item.species + ".png":item.photo} />
-                    </Center>
-                    <Box w={'20rem'} pl={{ base: 10 }}>
-                      <Heading as="h2" marginTop="1">
-                        <div>{item.name}</div>
-                      </Heading>
-                      <Text as="p" marginTop="2" fontSize="lg">
-                        <ul>
-                          <li>Species: {item.species}</li>
-                          <li>Breed: {item.breed}</li>
-                          <li>Birth: {item.birth
-                            ? item.birth.substring(5, 7) +
-                            "/" +
-                            item.birth.substring(8, 10) +
-                            "/" +
-                            item.birth.substring(0, 4)
-                            : item.birth}</li>
-                        </ul>
-                      </Text>
-                    </Box>
-                    <Box align={'center'}>
-                      <Button
-                        rounded={'full'}
-                        mt={{ base: '0', sm: '12' }}
-                        mb={{ base: '3', sm: '12' }}
-                        bg={'red.400'}
-                        _hover={{ bg: 'red.500' }}
-                        onClick={() => deleteP(item._id)}>
+                  <Box
+                    display="flex"
+                    flex="1"
+                    flexDirection="column"
+                    justifyContent="center"
+                    marginTop={{ base: "3", sm: "0" }}
+                    padding="2"
+                    paddingTop="3"
+                    paddingRight="0"
+                    backgroundColor="gray.50"
+                    borderRadius="md"
+                  >
+                    <Stack direction={["column", "row"]} spacing={6}>
+                      <Center>
+                        <Image
+                          h={"7rem"}
+                          w={"7rem"}
+                          ml={{ base: "0", sm: "10" }}
+                          src={
+                            item.photo === ""
+                              ? "/speciesIcons/" + item.species + ".png"
+                              : item.photo
+                          }
+                        />
+                      </Center>
+                      <Box w={"20rem"} pl={{ base: 10 }}>
+                        <Heading as="h2" marginTop="1">
+                          <div>{item.name}</div>
+                        </Heading>
+                        <Text as="p" marginTop="2" fontSize="lg">
+                          <ul>
+                            <li>Species: {item.species}</li>
+                            <li>Breed: {item.breed}</li>
+                            <li>
+                              Birth:{" "}
+                              {item.birth
+                                ? item.birth.substring(5, 7) +
+                                  "/" +
+                                  item.birth.substring(8, 10) +
+                                  "/" +
+                                  item.birth.substring(0, 4)
+                                : item.birth}
+                            </li>
+                          </ul>
+                        </Text>
+                      </Box>
+                      <Box align={"center"}>
+                        <Button
+                          rounded={"full"}
+                          mt={{ base: "0", sm: "12" }}
+                          mb={{ base: "3", sm: "12" }}
+                          bg={"red.400"}
+                          _hover={{ bg: "red.500" }}
+                          onClick={() => deleteP(item._id)}
+                        >
                           Delete
-                      </Button>
-                    </Box>
-                  </Stack>
+                        </Button>
+                      </Box>
+                    </Stack>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-          ))}
-        </Text>
-        ) : (
-          <Text>
-            No animals founded, add one!
+            ))}
           </Text>
+        ) : (
+          <Text>No animals founded, add one!</Text>
         )}
       </Container>
     </Container>
