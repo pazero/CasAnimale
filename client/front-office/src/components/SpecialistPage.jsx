8854cc4598cd79a1d70925071a89fa31706db850
Paolo Ceroni <paoloceroni01@gmail.com>
2023-01-26 12:14:34 +0100

    fixed prenotation overlaps and prenotation modal

--- client/front-office/src/components/SpecialistPage.jsx
+++ client/front-office/src/components/SpecialistPage.jsx
@@ -34,7 +34,7 @@ const SpecialistPage = () => {
   const [isOnline, setIsOnline] = useState(false);
   const [cities, setCities] = useState([]);
   const [rcities, setRCities] = useState([]);
-  const [selectedCity, setSelectedCity] = useState([]);
+  const [selectedCity, setSelectedCity] = useState();
   const [user, setUser] = useState([]);
   const token = Cookies.get("token");
 
@@ -582,7 +582,7 @@ const SpecialistPage = () => {
                     <button
                       className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
-                      onClick={() => setShowModal(false)}
+                      onClick={() => {setShowModal(false); setIsOnline(false); setStartDate(); setSelectedCity(); setStartTime()}}
                     >
                       Close
                     </button>
--- server/src/routes/Prenotation.js
+++ server/src/routes/Prenotation.js
@@ -11,7 +11,7 @@ function addHours(numOfHours, str) {
 }
 
 function dateOverlaps(a_start, a_end, b_start, b_end) {
-  if (a_start <= b_start && b_start <= a_end) return true; // b starts in a
+  if (a_start <= b_start && b_start < a_end) return true; // b starts in a
   if (a_start <= b_end && b_end <= a_end) return true; // b ends in a
   if (b_start < a_start && a_end < b_end) return true; // a in b
   return false;
