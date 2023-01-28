7826d40df18a0f973abb7c1e3ba4e57d96f76d7c
Paolo Ceroni <paoloceroni01@gmail.com>
2023-01-28 14:59:36 +0100

    quasi ok customer ma non si effettuano modifiche

--- server/src/server.js
+++ server/src/server.js
@@ -19,33 +19,29 @@ app.use(express.json());
 
 /* Route to static frontend apps, uncomment on producion */
 app.use("/b", express.static(path.join(__dirname, "../../client/back-office")));
-app.use(
-  "/f/",
-  express.static(path.join(__dirname, "../../client/front-office/build"))
-);
-app.get("/f/*", async (_, res) =>
-  res.end(
-    await fs.readFile(
-      path.join(__dirname, "../../client/front-office/build/index.html")
-    )
-  )
-);
-app.use("/g/", express.static(path.join(__dirname, "../../client/game/dist")));
-app.get("/g/*", async (_, res) =>
-  res.end(
-    await fs.readFile(path.join(__dirname, "../../client/game/dist/index.html"))
-  )
-);
+//app.use(
+//  "/f/",
+//  express.static(path.join(__dirname, "../../client/front-office/build"))
+//);
+//app.get("/f/*", async (_, res) =>
+//  res.end(
+//    await fs.readFile(
+//      path.join(__dirname, "../../client/front-office/build/index.html")
+//    )
+//  )
+//);
+//app.use("/g/", express.static(path.join(__dirname, "../../client/game/dist")));
+//app.get("/g/*", async (_, res) =>
+//  res.end(
+//    await fs.readFile(path.join(__dirname, "../../client/game/dist/index.html"))
+//  )
+//);
 
 /* Connect to DB */
 mongoose.connect(process.env.DB_CONNECTION);
 
 /* Api routes */
 require("./api")(app);
-
-app.use("/", async (_, res) =>
-  res.end(await fs.readFile(path.join(__dirname, "./index.html")))
-);
 
 /* Starting point */
 app.listen(process.env.PORT, console.log("Serving on port", process.env.PORT));
