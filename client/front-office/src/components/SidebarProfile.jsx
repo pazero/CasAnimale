d857b4ac85f8c033329cdfd778841d394b112cdf
geno <gabriele.genovese2@studio.unibo.it>
2023-01-29 22:11:59 +0100

    fixed sidebar in profile

--- client/front-office/src/components/SidebarProfile.jsx
+++ client/front-office/src/components/SidebarProfile.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { TbFish, TbUserCircle, TbCalendarEvent } from 'react-icons/tb';
-
+import { useNavigate } from "react-router-dom";
+import { TbFish, TbUserCircle, TbCalendarEvent } from "react-icons/tb";
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-1">
       <div className="hidden sm:flex">
@@ -14,22 +16,40 @@ export default function Sidebar() {
             <div className="flex flex-1">
               <ul className="pt-2 pb-4 space-y-1">
                 <li className="rounded-sm">
-                  <a href="/f/profile" className="flex items-center p-2 space-x-2 rounded-md">
+                  <div
+                    onClick={() => {
+                      navigate("/profile");
+                    }}
+                    className="flex items-center p-2 space-x-2 rounded-md"
+                  >
                     <TbUserCircle className="block h-6 w-6" alt="user icon" />
                     <span>Profile</span>
-                  </a>
+                  </div>
                 </li>
                 <li className="rounded-sm">
-                  <a href="/f/profile/pets" className="flex items-center p-2 space-x-2 rounded-md">
+                  <div
+                    onClick={() => {
+                      navigate("/profile/pets");
+                    }}
+                    className="flex items-center p-2 space-x-2 rounded-md"
+                  >
                     <TbFish className="block h-6 w-6" alt="fish icon" />
                     <span>Pets</span>
-                  </a>
+                  </div>
                 </li>
                 <li className="rounded-sm">
-                  <a href="/f/profile/prenotations" className="flex items-center p-2 space-x-3 rounded-md">
-                    <TbCalendarEvent className="block h-6 w-6" alt="calendar icon" />
+                  <div
+                    onClick={() => {
+                      navigate("/profile/prenotations");
+                    }}
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <TbCalendarEvent
+                      className="block h-6 w-6"
+                      alt="calendar icon"
+                    />
                     <span>Prenotations</span>
-                  </a>
+                  </div>
                 </li>
               </ul>
             </div>
@@ -38,23 +58,47 @@ export default function Sidebar() {
       </div>
       <div className="sm:hidden">
         <ul className="flex flex-row justify-items-stretch">
-          <li className="rounded-sm m-4 mr-4 basis-1/3" style={{flex: "1 1 auto"}}>
-            <a href="/f/profile" className="flex flex-row items-center space-x-3 rounded-md">
+          <li
+            className="rounded-sm m-4 mr-4 basis-1/3"
+            style={{ flex: "1 1 auto" }}
+          >
+            <div
+              onClick={() => {
+                navigate("/profile");
+              }}
+              className="flex flex-row items-center space-x-3 rounded-md"
+            >
               <TbUserCircle className="block h-6 w-6" alt="user icon" />
               <div>Profile</div>
-            </a>
+            </div>
           </li>
-          <li className="rounded-sm m-4 ml-4 basis-1/3" style={{flex: "1 1 auto"}}>
-            <a href="/f/profile/pets" className="flex flex-row items-center space-x-3 rounded-md">
-              <TbFish className="block h-6 w-6" alt="fish icon"/>
+          <li
+            className="rounded-sm m-4 ml-4 basis-1/3"
+            style={{ flex: "1 1 auto" }}
+          >
+            <div
+              onClick={() => {
+                navigate("/profile/pets");
+              }}
+              className="flex flex-row items-center space-x-3 rounded-md"
+            >
+              <TbFish className="block h-6 w-6" alt="fish icon" />
               <span>Pets</span>
-            </a>
+            </div>
           </li>
-          <li className="rounded-sm m-4 ml-4 basis-1/3" style={{flex: "1 1 auto"}}>
-            <a href="/f/profile/prenotations" className="flex flex-row items-center space-x-3 rounded-md">
-              <TbCalendarEvent className="block h-6 w-6" alt="fish icon"/>
+          <li
+            className="rounded-sm m-4 ml-4 basis-1/3"
+            style={{ flex: "1 1 auto" }}
+          >
+            <div
+              onClick={() => {
+                navigate("/profile/prenotations");
+              }}
+              className="flex flex-row items-center space-x-3 rounded-md"
+            >
+              <TbCalendarEvent className="block h-6 w-6" alt="fish icon" />
               <span>Prenotations</span>
-            </a>
+            </div>
           </li>
         </ul>
       </div>
