be858c2fcca7555ff7114b354ad6ed6c1d1d07e8
geno <gabriele.genovese2@studio.unibo.it>
2023-02-03 12:55:55 +0100

    utile per server là

--- client/game/src/router/utils.js
+++ client/game/src/router/utils.js
@@ -1,10 +1,10 @@
-const BEURL = "http://localhost:5000";
+const BEURL = "http://site212247.tw.cs.unibo.it";
 // if develpoment const FOURL = "http://localhost:3000"
 // if production const FOURL = BEURL + "/f";
-const FOURL = "http://localhost:3000" + "/f";
+const FOURL = BEURL + "/f";
 // if develpoment const GAMEURL = "http://localhost:5173"
 // if production const GAMEURL = BEURL + "/g";
-const GAMEURL = "http://localhost:5173"; //+ "/g";
+const GAMEURL = BEURL + "/g";
 const BOURL = BEURL + "/b";
 
 export default { BEURL, GAMEURL, BOURL, FOURL };
