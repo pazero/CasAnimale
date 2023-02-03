43048ce6bfb38d097d399a4ac131f04ead12402b
geno <gabriele.genovese2@studio.unibo.it>
2023-02-03 13:17:43 +0100

    memory accessibile e navbar

--- client/front-office/src/components/Navbar.jsx
+++ client/front-office/src/components/Navbar.jsx
@@ -47,10 +47,19 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar bg-blue-50" role="navigation" aria-label="Main" style={{ minHeight: "5%" }}>
+    <nav
+      className="navbar bg-blue-50"
+      role="navigation"
+      aria-label="Main"
+      style={{ minHeight: "5%" }}
+    >
       <div className="navbar-start">
         <div className="dropdown lg:hidden">
-          <label tabIndex={0} className="btn btn-ghost" aria-label="Dropdown menu">
+          <label
+            tabIndex={0}
+            className="btn btn-ghost"
+            aria-label="Dropdown menu"
+          >
             <span className="hidden">hamburger menu</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -75,7 +84,10 @@ const Navbar = () => {
               <a href={Const.GAMEURL}>Game Area</a>
             </li>
             <li tabIndex={0}>
-              <div className="justify-between" aria-label="community dropdown menu">
+              <div
+                className="justify-between"
+                aria-label="community dropdown menu"
+              >
                 Community
                 <svg
                   className="fill-current"
@@ -89,12 +101,20 @@ const Navbar = () => {
               </div>
               <ul className="p-2 border bg-base-100 z-10">
                 <li>
-                  <button onClick={() => {navigate("/forum");}}>
+                  <button
+                    onClick={() => {
+                      navigate("/forum");
+                    }}
+                  >
                     EccoloQua!
                   </button>
                 </li>
                 <li>
-                  <button onClick={() => {navigate("/findpartner");}}>
+                  <button
+                    onClick={() => {
+                      navigate("/findpartner");
+                    }}
+                  >
                     FindPartner
                   </button>
                 </li>
@@ -139,7 +159,11 @@ const Navbar = () => {
                   </button>
                 </li>
                 <li>
-                  <button onClick={() => {navigate("/boards");}}>
+                  <button
+                    onClick={() => {
+                      navigate("/boards");
+                    }}
+                  >
                     Leaderboard
                   </button>
                 </li>
@@ -159,8 +183,12 @@ const Navbar = () => {
                 </svg>
               </div>
               <ul className="p-2 border bg-base-100 z-10">
-                <li >
-                  <button onClick={() => {navigate("/compra");}}>
+                <li>
+                  <button
+                    onClick={() => {
+                      navigate("/compra");
+                    }}
+                  >
                     eCommerce
                   </button>
                 </li>
@@ -204,7 +232,11 @@ const Navbar = () => {
                   </button>
                 </li>
                 <li>
-                  <button onClick={() => {navigate("/petsitter");}}>
+                  <button
+                    onClick={() => {
+                      navigate("/petsitter");
+                    }}
+                  >
                     Pet Sitter
                   </button>
                 </li>
@@ -248,20 +280,32 @@ const Navbar = () => {
                   </button>
                 </li>
                 <li>
-                  <button onClick={() => {navigate("/grooming");}}>
+                  <button
+                    onClick={() => {
+                      navigate("/grooming");
+                    }}
+                  >
                     Grooming
                   </button>
                 </li>
               </ul>
             </li>
             <li>
-              <button onClick={() => { window.location.href = Const.BOURL;}}>
+              <button
+                onClick={() => {
+                  window.location.href = Const.BOURL;
+                }}
+              >
                 Reserved Section
               </button>
             </li>
           </ul>
         </div>
-        <a href={Const.FOURL} className="btn btn-ghost normal-case text-xl" aria-label="home button">
+        <a
+          href={Const.FOURL}
+          className="btn btn-ghost normal-case text-xl"
+          aria-label="CasAnimale home button"
+        >
           CasAnimale
         </a>
       </div>
@@ -271,7 +315,10 @@ const Navbar = () => {
             <a href={Const.GAMEURL}>Game Area</a>
           </li>
           <li tabIndex={0}>
-            <div className="justify-between" aria-label="community dropdown menu">
+            <div
+              className="justify-between"
+              aria-label="community dropdown menu"
+            >
               Community
               <svg
                 className="fill-current"
@@ -285,12 +332,20 @@ const Navbar = () => {
             </div>
             <ul className="p-2 border bg-base-100 z-10">
               <li>
-                <button onClick={() => {navigate("/forum");}}>
+                <button
+                  onClick={() => {
+                    navigate("/forum");
+                  }}
+                >
                   EccoloQua!
                 </button>
               </li>
               <li>
-                <button onClick={() => {navigate("/findpartner");}}>
+                <button
+                  onClick={() => {
+                    navigate("/findpartner");
+                  }}
+                >
                   FindPartner
                 </button>
               </li>
@@ -333,14 +388,21 @@ const Navbar = () => {
                 </button>
               </li>
               <li>
-                <button onClick={() => {navigate("/boards");}}>
+                <button
+                  onClick={() => {
+                    navigate("/boards");
+                  }}
+                >
                   Leaderboard
                 </button>
               </li>
             </ul>
           </li>
           <li tabIndex={0}>
-            <div className="justify-between" aria-label="Services dropdown menu">
+            <div
+              className="justify-between"
+              aria-label="Services dropdown menu"
+            >
               Services
               <svg
                 className="fill-current"
@@ -354,7 +416,11 @@ const Navbar = () => {
             </div>
             <ul className="p-2 border bg-base-100 z-10">
               <li>
-                <button onClick={() => {navigate("/compra");}}>
+                <button
+                  onClick={() => {
+                    navigate("/compra");
+                  }}
+                >
                   eCommerce
                 </button>
               </li>
@@ -397,7 +463,11 @@ const Navbar = () => {
                 </button>
               </li>
               <li>
-                <button onClick={() => {navigate("/petsitter");}}>
+                <button
+                  onClick={() => {
+                    navigate("/petsitter");
+                  }}
+                >
                   Pet Sitter
                 </button>
               </li>
@@ -440,14 +510,22 @@ const Navbar = () => {
                 </button>
               </li>
               <li>
-                <button onClick={() => {navigate("/grooming");}}>
+                <button
+                  onClick={() => {
+                    navigate("/grooming");
+                  }}
+                >
                   Grooming
                 </button>
               </li>
             </ul>
           </li>
           <li>
-            <button onClick={() => { window.location.href = Const.BOURL;}}>
+            <button
+              onClick={() => {
+                window.location.href = Const.BOURL;
+              }}
+            >
               Reserved Section
             </button>
           </li>
@@ -459,13 +537,18 @@ const Navbar = () => {
             <label
               tabIndex={0}
               className="btn font-semibold border-indigo-200 bg-indigo-200 text-indigo-900 hover:bg-indigo-200 hover:text-indigo-900 mr-2"
-              onClick={() => {navigate("/cart");}}>
+              onClick={() => {
+                navigate("/cart");
+              }}
+            >
               <TbShoppingCart className="block h-6 w-6" alt="cart icon" />
             </label>
 
             <button
               className="btn font-semibold border-indigo-200 bg-indigo-200 text-indigo-900 hover:bg-indigo-200 hover:text-indigo-900 mr-2"
-              onClick={() => {navigate("/profile");}}
+              onClick={() => {
+                navigate("/profile");
+              }}
             >
               <TbUserCircle className="block h-6 w-6 mr-1" alt="user icon" />
               Profile
@@ -478,12 +561,17 @@ const Navbar = () => {
             <label
               tabIndex={0}
               className="btn border-indigo-200 bg-indigo-200 text-indigo-900 hover:bg-indigo-200 hover:text-indigo-900"
-              onClick={() => {navigate("/cart");}}
+              onClick={() => {
+                navigate("/cart");
+              }}
             >
               <TbShoppingCart className="block h-6 w-6" alt="cart icon" />
             </label>
             <div className="dropdown ml-2">
-              <label tabIndex={0} className="btn font-semibold border-indigo-200 bg-indigo-200 text-indigo-900 hover:bg-indigo-200 hover:text-indigo-900 ">
+              <label
+                tabIndex={0}
+                className="btn font-semibold border-indigo-200 bg-indigo-200 text-indigo-900 hover:bg-indigo-200 hover:text-indigo-900 "
+              >
                 <TbUserCircle className="block h-6 w-6" alt="user icon" />
               </label>
               <ul
@@ -491,14 +579,16 @@ const Navbar = () => {
                 className="menu menu-compact dropdown-content mt-3 shadow bg-gray-100 p-0 rounded-box w-full"
               >
                 <li>
-                  <button onClick={() => {navigate("/profile");}}>
+                  <button
+                    onClick={() => {
+                      navigate("/profile");
+                    }}
+                  >
                     Profile
                   </button>
                 </li>
                 <li>
-                  <button onClick={logout}>
-                    Log out
-                  </button>
+                  <button onClick={logout}>Log out</button>
                 </li>
               </ul>
             </div>
@@ -507,7 +597,11 @@ const Navbar = () => {
       ) : (
         <div className="navbar-end">
           <div className="dropdown">
-              <label tabIndex={0} className="btn mr-2 font-semibold border-indigo-200 bg-indigo-200 text-indigo-900 hover:bg-indigo-200 hover:text-indigo-900" for="dropdown">
+            <label
+              tabIndex={0}
+              className="btn mr-2 font-semibold border-indigo-200 bg-indigo-200 text-indigo-900 hover:bg-indigo-200 hover:text-indigo-900"
+              for="dropdown"
+            >
               <span>login</span>
             </label>
             <ul
@@ -516,12 +610,20 @@ const Navbar = () => {
               className="menu menu-compact dropdown-content mt-3 shadow bg-gray-100 focus:indigo-200 p-0 rounded-box w-full"
             >
               <li>
-                <button onClick={() => {navigate("/login");}}>
+                <button
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
                   Sign in
                 </button>
               </li>
               <li>
-                <button onClick={() => {navigate("/register");}}>
+                <button
+                  onClick={() => {
+                    navigate("/register");
+                  }}
+                >
                   Sign up
                 </button>
               </li>
